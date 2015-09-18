b42f6bea3830e505dc9eea478d939cabf2eb4f15
Matthieu Monsch <monsch@alum.mit.edu>
2015-09-18 16:50:23 -0700

    Add first bundle.

---
+++ index.js
@@ -0,0 +1,11 @@
+/* jshint browser: true, browserify: true */
+
+(function () {
+  'use strict';
+
+  var avsc = require('avsc'),
+      buffer = require('buffer');
+
+  window.avsc = avsc;
+
+})();
