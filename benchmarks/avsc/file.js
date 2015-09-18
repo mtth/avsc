54ff62a4e5e9de511cec59a8e18f3c5e6af9f671
Matthieu Monsch <monsch@alum.mit.edu>
2015-09-18 07:24:23 -0700

    Switch decoder stream to using transform.

--- benchmarks/avsc/file.js
+++ benchmarks/avsc/file.js
@@ -3,9 +3,25 @@
 'use strict';
 
 var file = require('../../lib/file'),
-    fs = require('fs');
+    fs = require('fs'),
+    util = require('util');
 
 // Buffer.prototype.toJSON = function () { return this.toString('binary'); };
 
-var stream = fs.createReadStream('dat/users.avro');
-var reader = new file.Decoder(stream);
+var n = 0;
+var time;
+fs.createReadStream('benchmarks/dat/user-100000.avro')
+  .pipe(new file.Decoder())
+  .on('data', function (record) {
+    if (!time) {
+      time = process.hrtime();
+    }
+    n++;
+    if (record.name === null) {
+      this.emit('error', new Error('no name'));
+    }
+  })
+  .on('end', function () {
+    time = process.hrtime(time);
+    console.log(util.format('%s %s', n, time[0] + time[1] * 1e-9));
+  });
