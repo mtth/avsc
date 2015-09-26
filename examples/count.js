264f7a7d69960bf950d0322971f7edc17d9fe042
Matthieu Monsch <monsch@alum.mit.edu>
2015-09-26 15:48:57 -0700

    Update count example.

--- examples/count.js
+++ examples/count.js
@@ -15,19 +15,36 @@ var avsc = require('../lib'),
     util = require('util');
 
 
-var path = process.argv[2];
+var schema = process.argv[2];
+var path = process.argv[3];
+var type;
+if (path === undefined) {
+  path = schema;
+  type = undefined;
+} else {
+  type = avsc.parse(JSON.parse(schema));
+}
+
 if (!path) {
-  console.error('usage: node count.js PATH');
+  console.error('usage: node count.js [SCHEMA] PATH');
   process.exit(1);
 }
 
 var n = 0;
 var time;
-avsc.createReadStream(path)
-  .on('metadata', function () {
-    time = process.hrtime();
+avsc.decodeFile(path, {writerType: type})
+  .on('data', function () {
+    if (!time) {
+      // In the case of Avro container files, we could use the 'metadata' event
+      // to set the time instead.
+      time = process.hrtime();
+    }
+    n++;
   })
-  .on('data', function () { n++; })
+  .on('error', function (err) {
+    console.error(err.message);
+    process.exit(1);
+  })
   .once('end', function () {
     time = process.hrtime(time);
     console.log(util.format(
