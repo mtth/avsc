ba44c919969308e31a1a9d0701b9e50a8f683bee
Matthieu Monsch <monsch@alum.mit.edu>
2015-10-25 18:28:21 -0700

    Add official python and ruby implementation benchmarks.

---
+++ etc/benchmarks/scripts/encode/ruby-avro.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'avro'
+
+records = []
+file = File.open(ARGV[0], 'r+')
+dr = Avro::IO::DatumReader.new
+fr = Avro::DataFile::Reader.new(file, dr)
+schema = dr.writers_schema
+fr.each { |record| records << record }
+
+dw = Avro::IO::DatumWriter.new(schema)
+t = Time.now
+n = 0
+records.each { |record|
+  buffer = StringIO.new
+  encoder = Avro::IO::BinaryEncoder.new(buffer)
+  dw.write(record, encoder)
+  n += 1
+}
+puts (Time.now - t) / n
