fbf1c2e4b016fdc01d214c52e802a71da82bf34a
Matthieu Monsch <monsch@alum.mit.edu>
2015-12-06 17:40:49 -0800

    Start protocol implementation.
    API inspired by core events.

--- test/test_protocols.js
+++ test/test_protocols.js
@@ -4,7 +4,8 @@
 
 var protocols = require('../lib/protocols'),
     schemas = require('../lib/schemas'),
-    assert = require('assert');
+    assert = require('assert'),
+    stream = require('stream');
 
 
 suite('protocols', function () {
@@ -102,6 +103,75 @@ suite('protocols', function () {
         });
       });
     });
+
+  });
+
+  suite('Transmitter', function () {
+
+    test('read', function (done) {
+      var bufs = [
+        new Buffer([0, 1]),
+        new Buffer([2]),
+        new Buffer([]),
+        new Buffer([3, 4, 5]),
+        new Buffer([])
+      ];
+      var n = 0;
+      var readable = new stream.Readable({
+        read: function () {
+          var buf = bufs[n++];
+          this.push(buf ? frame(buf) : null);
+        }
+      });
+      var writable = new stream.Writable();
+      var messages = [];
+      var tm = new protocols.Transmitter(readable, writable);
+      tm.on('data', function (buf) { messages.push(buf); })
+        .on('end', function () {
+          assert.deepEqual(messages, [
+            new Buffer([0, 1, 2]),
+            new Buffer([3, 4, 5])
+          ]);
+          done();
+        });
+    });
+
+    test('write', function (done) {
+      var bufs = [];
+      var nReads = 0;
+      var readable = new stream.Readable({read: function () { nReads++; }});
+      var writable = new stream.Writable({
+        write: function (chunk, encoding, cb) {
+          bufs.push(chunk);
+          cb();
+        }
+      });
+      var tm = new protocols.Transmitter(readable, writable, {end: true});
+      tm.write(new Buffer([0, 1]));
+      tm.write(new Buffer([3]));
+      tm.end();
+      writable.on('finish', function () {
+          assert.equal(nReads, 1);
+          assert.deepEqual(
+            Buffer.concat(bufs),
+            Buffer.concat([
+              new Buffer([0, 1]),
+              new Buffer([]),
+              new Buffer([3]),
+              new Buffer([]),
+            ].map(frame))
+          );
+          done();
+        });
+    });
+
+    function frame(buf) {
+      var framed = new Buffer(buf.length + 4);
+      framed.writeInt32BE(buf.length);
+      buf.copy(framed, 4);
+      return framed;
+    }
+
   });
 
 });
