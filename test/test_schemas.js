f908ab72c231749c753ae4a0834b2a3375313ad9
Matthieu Monsch <monsch@alum.mit.edu>
2016-01-10 11:51:49 -0800

    Add IDL test.
    Also rename assemble `importer` option to more explicit `loader` (since
    it is not only used for imports but also for the file itself).

--- test/test_schemas.js
+++ test/test_schemas.js
@@ -7,19 +7,88 @@ if (process.browser) {
 }
 
 var schemas = require('../lib/schemas'),
-    assert = require('assert');
+    assert = require('assert'),
+    path = require('path');
+
+
+var DPATH = path.join(__dirname, 'dat');
 
 
 suite('schemas', function () {
 
   suite('assemble', function () {
 
-    test('missing file', function () {
+    var assemble = schemas.assemble;
+
+    test('missing file', function (done) {
+      assemble('./dat/foo', function (err) {
+        assert(err);
+        done();
+      });
     });
 
-  });
-
-  suite('JSON boundary', function () {
+    test('simple file', function (done) {
+      assemble(path.join(DPATH, 'hello.avdl'), function (err, attrs) {
+        assert.strictEqual(err, null);
+        assert.deepEqual(attrs, {
+          namespace: 'org.apache.avro.test',
+          protocol: 'Simple',
+          types: [
+            {
+              aliases: ['org.foo.KindOf'],
+              type: 'enum',
+              name: 'Kind',
+              symbols: ['FOO', 'BAR', 'BAZ']
+            },
+            {type: 'fixed', name: 'MD5', size: 16},
+            {
+              type: 'record',
+              name: 'TestRecord',
+              fields: [
+                {type: 'string', order: 'ignore', name: 'name'},
+                {type: 'Kind', order: 'descending', name: 'kind'},
+                {type: 'MD5', name: 'hash'},
+                {
+                  type: ['MD5', 'null'],
+                  aliases: ['hash'],
+                  name: 'nullableHash'
+                },
+                {type: {type: 'array', items: 'long'}, name: 'arrayOfLongs'}
+              ]
+            },
+            {
+              type: 'error',
+              name: 'TestError',
+              fields: [{type: 'string', name: 'message'}]
+            }
+          ],
+          messages: {
+            hello: {
+              response: 'string',
+              request: [{ type: 'string', name: 'greeting' }]
+            },
+            echo: {
+              response: 'TestRecord',
+              request: [{type: 'TestRecord', name: 'record'}]
+            },
+            add: {
+              response: 'int',
+              request: [
+                {type: 'int', name: 'arg1'},
+                {type: 'int', name: 'arg2'}
+              ]
+            },
+            echoBytes: {
+              response: 'bytes',
+              request: [{type: 'bytes', name: 'data'}]
+            },
+            error: {response: 'null', request: [], errors: ['TestError']},
+            ping: {response: 'null', request: [], 'one-way': true}
+          }
+        });
+        done();
+      });
+    });
 
   });
 
@@ -52,19 +121,27 @@ suite('schemas', function () {
 
     test('JSON', function () {
       [
-        {str: '324,', val: '324'},
-        {str: '3,', val: '3'},
-        {str: '-54,', val: '-54'},
-        {str: '-5.4)', val: '-5.4'},
-        {str: '"324",', val: '"324"'},
-        {str: '"hello \\"you\\""r', val: '"hello \\"you\\""'},
-        {str: '{}o', val: '{}'},
-        {str: '{"a": 1},', val: '{"a": 1}'},
-        {str: '[]', val: '[]'},
+        {str: '324,', val: 324},
+        {str: '3,', val: 3},
+        {str: '-54,', val: -54},
+        {str: '-5.4)', val: -5.4},
+        {str: '"324",', val: '324'},
+        {str: '"hello \\"you\\""r', val: 'hello "you"'},
+        {str: '{}o', val: {}},
+        {str: '{"a": 1},', val: {a: 1}},
+        {str: '[]', val: []},
       ].forEach(function (el) {
-        assert.equal(getToken(el.str, 'json').val, el.val);
+        assert.deepEqual(getToken(el.str, 'json').val, el.val);
       });
+    });
 
+    test('name', function () {
+      [
+        {str: 'hi', val: 'hi'},
+        {str: '`i3i`', val: 'i3i'}
+      ].forEach(function (el) {
+        assert.deepEqual(getToken(el.str).val, el.val);
+      });
     });
 
     function getToken(str, id) {
