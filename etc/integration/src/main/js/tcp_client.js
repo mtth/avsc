bfee4815f9ab1c7df6c199af8bb68ca541ee5ed5
Matthieu Monsch <monsch@alum.mit.edu>
2016-04-12 09:05:22 -0700

    Add Java Netty client implementation.
    IPC worked!
    
    [ci skip]

--- etc/integration/src/main/js/tcp_client.js
+++ etc/integration/src/main/js/tcp_client.js
@@ -9,9 +9,11 @@ var avro = require('../../../../../lib'),
 
 var protocol = avro.parse('./src/main/avro/math.avpr');
 var socket = net.createConnection({host: 'localhost', port: 65111});
-var ee = protocol.createEmitter(socket, function () { socket.destroy(); });
+var ee = protocol.createEmitter(socket)
+  .on('eot', function () { socket.destroy(); });
 
 protocol.emit('add', {pair: {left: 2, right: 5}}, ee, function (err, res) {
   assert.strictEqual(err, null);
   assert.equal(res, 7);
+  ee.destroy();
 });
