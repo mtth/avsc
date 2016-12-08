6bf9fefab47a39ae1a3d511749e368c29229dac9
Matthieu Monsch <monsch@alum.mit.edu>
2016-12-07 22:09:05 -0800

    Fix browserify file-system shim.

--- etc/browser/lib/files.js
+++ etc/browser/lib/files.js
@@ -6,10 +6,18 @@
 
 function createError() { return new Error('unsupported in the browser'); }
 
+function createImportHook() {
+  return function (fpath, kind, cb) { cb(createError()); };
+}
+
+function createSyncImportHook() {
+  return function () { throw createError(); };
+}
+
 
 module.exports = {
-  createImportHook: function (fpath, kind, cb) { cb(createError()); },
-  createSyncImportHook: function () { throw createError(); },
+  createImportHook: createImportHook,
+  createSyncImportHook: createSyncImportHook,
   existsSync: function () { return false; },
   readFileSync: function () { throw createError(); }
 };
