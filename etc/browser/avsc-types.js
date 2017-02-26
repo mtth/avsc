142b9a36ee162b3a94abd1d809126806b3c7a015
Matthieu Monsch <monsch@alum.mit.edu>
2017-02-26 10:19:28 -0800

    Integrate buffer shim.

--- etc/browser/avsc-types.js
+++ etc/browser/avsc-types.js
@@ -11,32 +11,6 @@
 var types = require('../../lib/types');
 
 
-// Since there are no utf8 and binary functions on browserify's `Buffer`, we
-// must patch in tap methods using the generic slice and write methods.
-(function polyfillTap() {
-  var Tap = require('../../lib/utils').Tap;
-
-  Tap.prototype.readString = function () {
-    var len = this.readLong();
-    var pos = this.pos;
-    var buf = this.buf;
-    this.pos += len;
-    if (this.pos > buf.length) {
-      return;
-    }
-    return this.buf.slice(pos, pos + len).toString();
-  };
-
-  Tap.prototype.writeBinary = function (s, len) {
-    var pos = this.pos;
-    this.pos += len;
-    if (this.pos > this.buf.length) {
-      return;
-    }
-    this.buf.write(s, pos, len, 'binary');
-  };
-})();
-
 /** Basic parse method, only supporting JSON parsing. */
 function parse(any, opts) {
   var schema;
