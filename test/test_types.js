94fea36f7b21f00184d38f3c025fe92767601763
Matthieu Monsch <matthieu.monsch@gmail.com>
2019-06-29 08:48:45 -0700

    Fix nested logical type resolution inside unions
    More context here: https://github.com/mtth/avsc/issues/243

--- test/test_types.js
+++ test/test_types.js
@@ -2843,9 +2843,9 @@ suite('types', function () {
         ['null', {type: 'int', logicalType: 'age'}],
         {logicalTypes: logicalTypes, wrapUnions: true}
       );
-      var resolver = t.createResolver(t)
+      var resolver = t.createResolver(t);
       var v = {'int': 34};
-      assert.deepEqual(t.fromBuffer(t.toBuffer(v), resolver), v)
+      assert.deepEqual(t.fromBuffer(t.toBuffer(v), resolver), v);
     });
 
     test('even integer', function () {
@@ -2967,29 +2967,29 @@ suite('types', function () {
         };
       }
 
+      /**
+      * A generic union type which exposes its values directly.
+      *
+      * This implementation predates the existence of the
+      * `UnwrappedUnionType` currently in the built-in types. It can still be
+      * used as an example to implement custom unwrapped unions (which would
+      * be able to cover ambiguous unions).
+      *
+      */
+      function UnwrappedUnionType(schema, opts) {
+        LogicalType.call(this, schema, opts);
+      }
+      util.inherits(UnwrappedUnionType, LogicalType);
+
+      UnwrappedUnionType.prototype._fromValue = function (val) {
+        return val === null ? null : val[Object.keys(val)[0]];
+      };
+
+      UnwrappedUnionType.prototype._toValue = function (any) {
+        return this.getUnderlyingType().clone(any, {wrapUnions: true});
+      };
+
       test('unwrapped', function () {
-
-        /**
-        * A generic union type which exposes its values directly.
-        *
-        * This implementation predates the existence of the
-        * `UnwrappedUnionType` currently in the built-in types. It can still be
-        * used as an example to implement custom unwrapped unions (which would
-        * be able to cover ambiguous unions).
-        *
-        */
-        function UnwrappedUnionType(schema, opts) {
-          LogicalType.call(this, schema, opts);
-        }
-        util.inherits(UnwrappedUnionType, LogicalType);
-
-        UnwrappedUnionType.prototype._fromValue = function (val) {
-          return val === null ? null : val[Object.keys(val)[0]];
-        };
-
-        UnwrappedUnionType.prototype._toValue = function (any) {
-          return this.getUnderlyingType().clone(any, {wrapUnions: true});
-        };
 
         var t1 = Type.forSchema(
           schema,
@@ -3004,6 +3004,39 @@ suite('types', function () {
           {Person: {name: 'Ann', age: {'int': 23}}}
         );
 
+      });
+
+      test('unwrapped with nested logical types', function () {
+
+        var schema = [
+          'null',
+          {
+            type: 'record',
+            name: 'Foo',
+            fields: [
+              {
+                name: 'date',
+                type: [
+                  'null',
+                  {type: 'long', logicalType: 'timestamp-millis'}
+                ]
+              }
+            ]
+          }
+        ];
+        var t1 = Type.forSchema(
+          schema,
+          {
+            logicalTypes: {'timestamp-millis': DateType},
+            typeHook: createUnionTypeHook(UnwrappedUnionType),
+            wrapUnions: true,
+          }
+        );
+        var obj = {date: new Date(1234)};
+        assert(t1.isValid(obj));
+        var buf = t1.toBuffer(obj);
+        var t2 = Type.forSchema(schema, {wrapUnions: true});
+        assert.deepEqual(t2.fromBuffer(buf), {Foo: {date: {long: 1234}}});
       });
 
       test('optional', function () {
